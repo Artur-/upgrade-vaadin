5e48cb010967519a200f9ef8817c728584aec86b
Artur Signell <artur@vaadin.com>
2021-10-04 13:08:42 +0300

    Modify instead of rewriting the whole file

--- index.js
+++ index.js
@@ -1,102 +1,130 @@
 #!/usr/bin/env node
 
-const { convert } = require('xmlbuilder2');
+const xmldoc = require('xmldoc');
 
 const fs = require('fs');
 
-const replacePlugin = (plugin) => {
-  if (plugin.artifactId === 'vaadin-maven-plugin') {
-    plugin.artifactId = 'flow-maven-plugin';
-    plugin.version = '${flow.version}';
-  }
-  return plugin;
+const replaceValue = (node, text) => {
+  replacements.push({ position: node.position, length: node.val.length, replacement: text });
 };
-const replacePlugins = (plugins) => {
-  if (Array.isArray(plugins)) {
-    profile.build.plugins.plugin = profile.build.plugins.plugin.map((plugin) => replacePlugin(plugin));
-  } else if (plugins['#']) {
-    plugins['#'] = plugins['#'].map((maybePlugin) => {
-      if (maybePlugin.plugin) {
-        maybePlugin.plugin = replacePlugin(maybePlugin.plugin);
-      }
-      return maybePlugin;
-    });
-  } else {
-    plugins.plugin = replacePlugin(plugins.plugin);
-  }
-  return plugins;
+const addAsFirstChild = (node, text) => {
+  replacements.push({ position: node.position, length: 0, replacement: text });
 };
-const replaceProfile = (profile) => {
-  profile.build.plugins = replacePlugins(profile.build.plugins);
-  return profile;
+const addAfter = (node, data, text) => {
+  const find = `${node.name}>`;
+  const endOfNode = data.indexOf(find, node.position) + find.length;
+  replacements.push({
+    position: endOfNode,
+    length: 0,
+    replacement: text,
+  });
 };
 
-try {
-  var myArgs = process.argv.slice(2);
-  let flowVersion;
-  let vaadinVersion;
-  if (myArgs.length === 0) {
-    console.error('Usage: change-vaadin <vaadin version> [flow version]');
+const replacePlugin = (node) => {
+  const buildPlugins = node.descendantWithPath('build.plugins');
+  buildPlugins.childrenNamed('plugin').forEach((plugin) => {
+    const artifactId = plugin.descendantWithPath('artifactId');
+    if (artifactId.val === 'vaadin-maven-plugin') {
+      replaceValue(artifactId, 'flow-maven-plugin');
+      replaceValue(plugin.descendantWithPath('version'), '${flow.version}');
+    }
+  });
+};
+var myArgs = process.argv.slice(2);
+let flowVersion;
+let vaadinVersion;
+if (myArgs.length === 0) {
+  console.error('Usage: change-vaadin <vaadin version> [flow version]');
+  return;
+}
+if (myArgs[0]) {
+  vaadinVersion = myArgs[0];
+}
+if (myArgs[1]) {
+  flowVersion = myArgs[1];
+}
+
+const data = fs.readFileSync('pom.xml', 'utf8');
+var document = new xmldoc.XmlDocument(data);
+const replacements = [];
+const indent = '    ';
+
+// Properties
+const properties = document.descendantWithPath('properties');
+let vaadinVersionNode = undefined;
+let flowVersionHandled = false;
+properties.eachChild((property) => {
+  if (property.name === 'vaadin.version') {
+    replaceValue(property, vaadinVersion);
+    vaadinVersionNode = property;
+  } else if (property.name === 'flow.version') {
+    replacements.push({ position: property.position, length: property.val.length, replacement: flowVersion });
+    flowVersionHandled = true;
+  }
+});
+
+if (vaadinVersionNode === undefined) {
+  console.error('No vaadin.version property found. Is this a Vaadin project?');
+  return;
+}
+
+if (!flowVersionHandled) {
+  addAfter(vaadinVersionNode, data, '\n' + indent.repeat(2) + `<flow.version>${flowVersion}</flow.version>`);
+}
+
+// Dependency management
+const dependencyManagement = document.descendantWithPath('dependencyManagement.dependencies');
+let flowBomHandled = false;
+let vaadinBomNode = undefined;
+
+dependencyManagement.eachChild((dependency) => {
+  if (dependency.valueWithPath('artifactId') === 'vaadin-bom') {
+    vaadinBomNode = dependency;
+  } else if (dependency.valueWithPath('artifactId') === 'flow-bom') {
+    flowBomHandled = true;
     return;
   }
-  if (myArgs[0]) {
-    vaadinVersion = myArgs[0];
-  }
-  if (myArgs[1]) {
-    flowVersion = myArgs[1];
-  }
-
-  const data = fs.readFileSync('pom.xml', 'utf8');
-  const json = convert(data, { format: 'object' });
-
-  const project = json.project;
-  if (vaadinVersion) {
-    project.properties['vaadin.version'] = vaadinVersion;
-  }
-  if (vaadinVersion) {
-    project.properties['flow.version'] = flowVersion;
-  }
-  if (flowVersion) {
-    let depManDep = project.dependencyManagement.dependencies.dependency;
-    let depManDepFound = false;
-    if (Array.isArray(depManDep)) {
-      depManDep.forEach((d) => {
-        if (d.artifactId === 'flow-bom') {
-          d.version = '${flow.version}';
-          depManDepFound = true;
-        }
-      });
-    } else if (depManDep.artifactId === 'flow-bom') {
-      depManDep.version = '${flow.version}';
-      depManDepFound = true;
-    }
-    if (!depManDepFound) {
-      newDepManDep = {
-        groupId: 'com.vaadin',
-        artifactId: 'flow-bom',
-        version: '${flow.version}',
-        type: 'pom',
-        scope: 'import',
-      };
-      if (Array.isArray(depManDep)) {
-        depManDep = [newDepManDep, ...depManDep];
-      } else {
-        depManDep = [newDepManDep, depManDep];
-      }
-    }
-    project.dependencyManagement.dependencies.dependency = depManDep;
-
-    project.build.plugins = replacePlugins(project.build.plugins);
-
-    if (Array.isArray(project.profiles.profile)) {
-      project.profiles.profile = project.profiles.profile.map((profile) => replaceProfile(profile));
-    } else {
-      project.profiles.profile = replaceProfile(project.profiles.profile);
-    }
-  }
-
-  const xml = convert(json, { format: 'xml', prettyPrint: true, indent: '    ' });
-  fs.writeFileSync('pom.xml', xml);
-} catch (err) {
-  console.error(err);
+});
+if (vaadinBomNode === undefined) {
+  console.log('No vaadin-bom dependency found. Is this a Vaadin project?');
+  return;
 }
+if (!flowBomHandled) {
+  // Must be first
+  const dep = `\n${indent.repeat(3)}<dependency>
+${indent.repeat(4)}<groupId>com.vaadin</groupId>
+${indent.repeat(4)}<artifactId>flow-bom</artifactId>
+${indent.repeat(4)}<version>\${flow.version}</version>
+${indent.repeat(4)}<type>pom</type>
+${indent.repeat(4)}<scope>import</scope>
+${indent.repeat(3)}</dependency>`;
+  addAsFirstChild(dependencyManagement, dep);
+}
+
+// Plugin name and version
+replacePlugin(document);
+
+const profiles = document.descendantWithPath('profiles');
+profiles.childrenNamed('profile').forEach((profile) => {
+  replacePlugin(profile);
+});
+
+// Replace and write
+replacements.sort((a, b) => {
+  if (a.position < b.position) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+let output = data;
+replacements.forEach((replacement) => {
+  //   console.log(
+  //     'Replacing ' + output.substr(replacement.position, replacement.length) + ' with ' + replacement.replacement
+  //   );
+  output =
+    output.substr(0, replacement.position) +
+    replacement.replacement +
+    output.substr(replacement.position + replacement.length);
+});
+fs.writeFileSync('pom.xml.out', output);
